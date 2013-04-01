3e25308276e14551b9e726f6d2946ec05de7a2aa
Catalina De la cuesta <delacuesta@hotmail.com>
2013-04-01 16:28:42 -0500

    Primero

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hello::Application.config.secret_token = '768e1f66551baeacea2c2ef3be72a853462a56e94fd79f9da211dac8248fc88c1c297b014ae2db13a3fd38e5016a9d429bd071d5498893cd3fbe459eba34f3e2'
---
+++ db/migrate/20130401211809_crear_usuarios.rb
@@ -0,0 +1,7 @@
+class CrearUsuarios < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+end
